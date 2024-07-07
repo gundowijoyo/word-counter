27df6952e5fc2ee8c073185fa8a26d38362f3d79
gun-joyo <133162689+gundowijoyo@users.noreply.github.com>
2024-07-07 21:52:42 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
+#membuat fungsi unfuk menghitung jumlah kata
 def word_count(text)
   text.split.size
 end
-
+#meminta input dan mencetak jumlah kata
 puts "Masukkan teks:"
 input_text = gets.chomp
 puts "Jumlah kata: #{word_count(input_text)}"
